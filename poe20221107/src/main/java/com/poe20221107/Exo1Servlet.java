2d931c1d1c790ca3d591df7dacef9175e6eec51d
Tim HOANG <ht.nghiem0306@gmail.com>
2022-12-05 11:16:50 +0100

    add session to exo1

--- poe20221107/src/main/java/com/poe20221107/Exo1Servlet.java
+++ poe20221107/src/main/java/com/poe20221107/Exo1Servlet.java
@@ -8,6 +8,7 @@
 //-> limiter le nombre de tentatives autorisées pour chaque partie
 package com.poe20221107;
 import java.io.IOException;
+import java.util.HashSet;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,19 +18,31 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/exo1")
 public class Exo1Servlet extends HttpServlet{
-    int random_int = (int) Math.floor(Math.random() * 11);
+//    int random_int = (int) Math.floor(Math.random() * 11);
     int nombreDeTentative =4;
-    int tentative = 0;
+//    int tentative = 0;
+    
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException{
         
         String number = request.getParameter("number");
-        try{
-            
-            
+       
+            try{
+            // On recupère la position du joueur
             int number1 =  Integer.parseInt(number);
+            
+            //on recupere les informations de la partie
+            Integer random_int = (Integer) request.getSession().getAttribute("random_int"); 
+            Integer tentative = (Integer) request.getSession().getAttribute("tentative");
+            
+           // Initialisation de la partie
+           if(random_int == null){
+               random_int = (int) Math.floor(Math.random() * 11);
+               tentative = 0;
+           }
             response.getWriter().append("<h1>nombre a deviner: "+random_int+"<h1>");
+            
             if(tentative < nombreDeTentative){
                 
                 if(number1==random_int){
@@ -57,6 +70,10 @@ public class Exo1Servlet extends HttpServlet{
                 response.getWriter().append( "<h1>veuillez saisir un nombre pour recommencer<h1>");
                 
             }tentative++;
+            request.getSession().setAttribute("random_int",random_int);
+            request.getSession().setAttribute("tentative", tentative);
+            
+                
         }catch(NumberFormatException e){
             response.getWriter().append("<h1>erreur, il faut entrer un nombre <h1>");
         }
