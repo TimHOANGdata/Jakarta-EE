d7e3261569713aeb357604025e444a3352308bbb
Tim HOANG <ht.nghiem0306@gmail.com>
2022-12-05 09:48:41 +0100

    add session to querryParam

--- poe20221107/src/main/java/com/poe20221107/querryParamServlet.java
+++ poe20221107/src/main/java/com/poe20221107/querryParamServlet.java
@@ -13,9 +13,14 @@ public class querryParamServlet extends HttpServlet{
         throws IOException{
         
         String prenom = request.getParameter("prenom");
+        if(prenom == null){
+             prenom = (String)request.getSession().getAttribute("prenomDansLaSession");
+        //getAtrribute retourne object donc il faut un cast en String
+        //getAttribute permet de retourner l'identifiant enrégistré
+        }
+        request.getSession().setAttribute("prenomDansLaSession",prenom);
+        //setAtrribute permet d'enregistrer l'identifiant dans le cookie
         response.getWriter().append("<p>tu t'appelles</p>");
         response.getWriter().append("<p>"+prenom+"</p>");
     }
 }
-//Utiliser query param pour les request dynamique.
-//http://localhost:8080/poe20221107/param?prenom=Tim
